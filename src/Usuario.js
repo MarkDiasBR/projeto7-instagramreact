6d9b0454f73e4cd327984778e1130df362a1a567
MarkDiasBR <117422129+MarkDiasBR@users.noreply.github.com>
2023-02-10 13:56:55 -0300

    Mudar usuário

--- src/Usuario.js
+++ src/Usuario.js
@@ -1,11 +1,28 @@
+import { useState } from 'react'
+
 export default function Usuario() {
+    const [usuario, setUsuario] = useState("catanacomics");
+    const [urlFoto, setUrlFoto] = useState("assets/img/catanacomics.svg")
+
+    function mudarUsuario() {
+        
+        const novoUsuario = prompt("Digite o nome do novo usuário")
+        const novaUrlFoto = prompt("Digite a URL da nova foto")
+        const falsy = [null, NaN, undefined, false, ""]
+
+        if ( !( falsy.includes(novoUsuario) ) || !( falsy.includes(novaUrlFoto) ) ) {
+            setUsuario(novoUsuario)
+            setUrlFoto(novaUrlFoto) 
+        }
+    }
+
     return (
         <div class="usuario">
-            <img data-test="profile-image" src="assets/img/catanacomics.svg" alt="imagem de perfil"/>
+            <img data-test="profile-image" src={urlFoto} alt="imagem de perfil"/>
             <div class="texto">
                 <span>
-                    <strong data-test="name">catanacomics</strong>
-                    <ion-icon data-test="edit-name" name="pencil"></ion-icon>
+                    <strong data-test="name">{usuario}</strong>
+                    <ion-icon onClick={mudarUsuario} data-test="edit-name" name="pencil"></ion-icon>
                 </span>
             </div>
         </div>
